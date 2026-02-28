6f1d67121d94b23f7b370e6253906eb1f2450c52
FrostyC <coltonslashfriends@gmail.com>
2026-02-28 01:51:12 +0000

    changes

--- script.js
+++ script.js
@@ -56,17 +56,7 @@ window.addEventListener('scroll', () => {
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     // Skip the play button to allow it to function correctly
     if (anchor.id !== 'playButton') {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            const target = document.querySelector(this.getAttribute('href'));
-            if (target) {
-                const offsetTop = target.offsetTop - 80;
-                window.scrollTo({
-                    top: offsetTop,
-                    behavior: 'smooth'
-                });
-            }
-        });
+        anchor.addEventListener('click', smoothScrollHandler);
     }
 });
 
